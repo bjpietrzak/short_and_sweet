a1e7f84fc15c1b7920719b9c023a50d94d8bdffb
bpietrzak <75700168+bjpietrzak@users.noreply.github.com>
2023-12-19 23:29:57 +0100

    more topics

--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -46,12 +46,26 @@ async def request_inference_bertopic() -> BertopicInferenceResponse:
         topics={
             0:"phone_app_service_picture",
             1:"instagram_time_bad_habbit",
-            2:"cant_open_camera_error"
+            2:"cant_open_camera_error",
+            3:"instagram_time_bad_habbit",
+            4:"cant_open_camera_error",
+            5:"instagram_time_bad_habbit",
+            6:"cant_open_camera_error",
+            7:"instagram_time_bad_habbit",
+            8:"cant_open_camera_error",
+            9:"instagram_time_bad_habbit",
         },
         counts={
-            0: 990,
-            1: 201,
-            2: 40
+            0: 740,
+            1: 350,
+            2: 210,
+            3: 200,
+            4: 190,
+            5: 180,
+            6: 170,
+            7: 160,
+            8: 150,
+            9: 140,
         })
     return response
 
